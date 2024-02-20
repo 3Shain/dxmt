866ea0a9379352dd4c58c02b383c6435300afa13
3Shain <san3shain@outlook.com>
2024-02-20 00:34:10 +0000

    ci: build script

--- configure.sh
+++ configure.sh
@@ -10,18 +10,25 @@ curl -SL https://github.com/mstorsjo/llvm-mingw/releases/download/20231017/llvm-
 tar -zvxf ./llvm.tar.xz -C ./toolchains
 rm ./llvm.tar.xz
 
-## configure
-# export PATH="$PATH:$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal/bin"
-# meson setup --cross-file build-win64.txt --native-file build-osx.txt build
-ß
-## build
-# meson compile -C build
 
 # setup llvm
 git clone --depth 1 https://github.com/llvm/llvm-project.git toolchains/llvm-project 
 mkdir -p ./toolchains/llvm-build
-cmake -B./toolchains/llvm-build -S./toolchains/llvm-project/llvm -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_INSTALL_PREFIX=$(pwd)/toolchains/llvm -DLLVM_HOST_TRIPLE=x86_64-w64-mingw32 -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSROOT="$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal" -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -G Ninja
-cd ./toolchains/llvm-build
+cmake -B./toolchains/llvm-build \
+-S./toolchains/llvm-project/llvm \ 
+-DCMAKE_SYSTEM_NAME=Windows -DCMAKE_INSTALL_PREFIX=$(pwd)/toolchains/llvm \
+-DLLVM_HOST_TRIPLE=x86_64-w64-mingw32 -DLLVM_TARGETS_TO_BUILD=X86 \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_SYSROOT="$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal" \
+-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+-G Ninja
+pushd ./toolchains/llvm-build
 ninja
 ninja install
-cd ../..
+popd
+
+## configure
+# export PATH="$PATH:$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal/bin"
+# meson setup --cross-file build-win64.txt --native-file build-osx.txt build
+## build
+# meson compile -C build
