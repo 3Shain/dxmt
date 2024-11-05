5118ccade6476e80b8c61a5a67f9a40cebee3f69
3Shain <san3shain@outlook.com>
2024-11-05 20:46:46 +0800

    ci: improve build process on arm macOS

--- configure.sh
+++ configure.sh
@@ -11,19 +11,17 @@ tar -zvxf ./llvm.tar.xz -C ./toolchains
 rm ./llvm.tar.xz
 
 
-# setup llvm
-# I don't really know what version of llvm it should be
 git clone --depth 1 --branch llvmorg-15.0.7 https://github.com/llvm/llvm-project.git toolchains/llvm-project 
 mkdir -p ./toolchains/llvm-build
 cmake -B ./toolchains/llvm-build -S ./toolchains/llvm-project/llvm -DCMAKE_SYSTEM_NAME=Windows \
   -DCMAKE_INSTALL_PREFIX="$(pwd)/toolchains/llvm" \
   -DLLVM_HOST_TRIPLE=x86_64-w64-mingw32 \
-  -DLLVM_TARGETS_TO_BUILD=X86 \
   -DLLVM_ENABLE_ASSERTIONS=On \
   -DLLVM_ENABLE_ZSTD=Off \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_TARGETS_TO_BUILD="" \
   -DLLVM_BUILD_TOOLS=Off \
+  -DLLVM_NATIVE_TOOL_DIR="$(brew --prefix llvm@15)/bin" \
   -DCMAKE_SYSROOT="$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal" \
   -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
   -DBUG_REPORT_URL="https://github.com/3Shain/dxmt" \
@@ -38,11 +36,14 @@ popd
 mkdir -p ./toolchains/llvm-darwin-build
 cmake -B ./toolchains/llvm-darwin-build -S ./toolchains/llvm-project/llvm \
   -DCMAKE_INSTALL_PREFIX="$(pwd)/toolchains/llvm-darwin" \
+  -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+  -DLLVM_HOST_TRIPLE=x86_64-apple-darwin \
   -DLLVM_ENABLE_ASSERTIONS=On \
   -DLLVM_ENABLE_ZSTD=Off \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_TARGETS_TO_BUILD="" \
   -DLLVM_BUILD_TOOLS=Off \
+  -DLLVM_NATIVE_TOOL_DIR="$(brew --prefix llvm@15)/bin" \
   -DBUG_REPORT_URL="https://github.com/3Shain/dxmt" \
   -DPACKAGE_VENDOR="DXMT" \
   -DLLVM_VERSION_PRINTER_SHOW_HOST_TARGET_INFO=Off \
