afecc586b03f05314e06cd1c59cbabb712839a2b
3Shain <san3shain@outlook.com>
2024-11-18 18:46:28 +0800

    refactor: dynamic texture implementation

--- src/dxmt/dxmt_buffer_pool.cpp
+++ src/dxmt/dxmt_buffer_pool.cpp
@@ -1,14 +1,12 @@
-#include "./dxmt_buffer_pool.hpp"
-#include "Metal/MTLDevice.hpp"
+#include "dxmt_buffer_pool.hpp"
 #include "dxmt_buffer.hpp"
+#include "dxmt_texture.hpp"
 
 namespace dxmt {
 
 void
-BufferPool::GetNext(
-    uint64_t currentSeqId, uint64_t coherentSeqId, MTL::Buffer **next, uint64_t *gpuAddr, void **cpuAddr
-) {
-  fifo.push(QueueEntry{.buffer = *next, .gpu_addr = *gpuAddr, .cpu_addr = *cpuAddr, .will_free_at = currentSeqId});
+BufferPool2::Rename(uint64_t currentSeqId, uint64_t coherentSeqId) {
+  Rc<BufferAllocation> alloc;
   for (;;) {
     if (fifo.empty()) {
       break;
@@ -17,22 +15,22 @@ BufferPool::GetNext(
     if (entry.will_free_at > coherentSeqId) {
       break;
     }
-    *next = entry.buffer;
-    *gpuAddr = entry.gpu_addr;
-    *cpuAddr = entry.cpu_addr;
+    alloc = std::move(entry.allocation);
     fifo.pop();
-    return;
+    break;
   }
-
-  auto buffer = device->newBuffer(buffer_len, options);
-  *next = buffer;
-  *gpuAddr = buffer->gpuAddress();
-  *cpuAddr = buffer->contents();
+  if (!alloc.ptr())
+    alloc = buffer_->allocate(flags_);
+  auto old = buffer_->rename(std::move(alloc));
+  if (old.ptr()) {
+    fifo.push(QueryEntry{.allocation = std::move(old), .will_free_at = currentSeqId});
+  }
 }
 
+
 void
-BufferPool2::Rename(uint64_t currentSeqId, uint64_t coherentSeqId) {
-  Rc<BufferAllocation> alloc;
+DynamicTexturePool2::Rename(uint64_t currentSeqId, uint64_t coherentSeqId) {
+  Rc<TextureAllocation> alloc;
   for (;;) {
     if (fifo.empty()) {
       break;
