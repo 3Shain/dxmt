4abd7b6b389757bbe4fc82b4cef2672d1172b755
3Shain <san3shain@outlook.com>
2025-01-24 10:12:04 +0800

    fix(d3d11): handle incorrect SysMemPitch when initializing 2d texture

--- src/d3d11/d3d11_texture.cpp
+++ src/d3d11/d3d11_texture.cpp
@@ -1,7 +1,5 @@
-#include "dxmt_names.hpp"
-#include "Metal/MTLTexture.hpp"
 #include "d3d11_texture.hpp"
-#include "d3d11_device.hpp"
+#include "mtld11_resource.hpp"
 
 namespace dxmt {
 
@@ -34,11 +32,17 @@ void InitializeTextureData(MTLD3D11Device * pDevice, MTL::Texture *target,
   uint32_t width = Desc.Width;
   uint32_t height = Desc.Height;
   for (uint32_t level = 0; level < Desc.MipLevels; level++) {
+    uint32_t bytes_per_row = 0, _, __;
+    GetLinearTextureLayout(pDevice, Desc, level, bytes_per_row, _, __, false);
     for (uint32_t slice = 0; slice < Desc.ArraySize; slice++) {
       auto idx = D3D11CalcSubresource(level, slice, Desc.MipLevels);
       auto region = MTL::Region::Make2D(0, 0, width, height);
-      target->replaceRegion(region, level, slice, subresources[idx].pSysMem,
-                            subresources[idx].SysMemPitch, 0);
+      if (subresources[idx].SysMemPitch < bytes_per_row) {
+        WARN("RowPitch provided ", subresources[idx].SysMemPitch, ", expect at least ", bytes_per_row);
+      }
+      target->replaceRegion(
+          region, level, slice, subresources[idx].pSysMem, std::max(subresources[idx].SysMemPitch, bytes_per_row), 0
+      );
     }
     width = std::max(1u, width >> 1);
     height = std::max(1u, height >> 1);
