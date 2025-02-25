ce561d91fb214b6f22c42d9ea0752208ec183026
3Shain <san3shain@outlook.com>
2025-02-25 14:14:17 +0800

    feat(d3d11): introduce `IMTLD3D11DeviceExt` interface

--- src/d3d11/d3d11_interfaces.hpp
+++ src/d3d11/d3d11_interfaces.hpp
@@ -27,3 +27,8 @@ DEFINE_COM_INTERFACE("43ace3ce-1956-448b-a4eb-aee68bdeb283",
    virtual void STDMETHODCALLTYPE BeginUAVOverlap() = 0;
    virtual void STDMETHODCALLTYPE EndUAVOverlap() = 0;
 };
+
+DEFINE_COM_INTERFACE("efc77ae6-2179-4c0a-b844-7661ca0dcde7", IMTLD3D11DeviceExt)
+    : public IUnknown {
+  virtual void STDMETHODCALLTYPE SetShaderExtensionSlot(UINT Slot) = 0;
+};
