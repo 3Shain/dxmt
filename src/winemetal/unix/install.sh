622f6cd2992cf4f2e84eb4b981a7771b5ed6b67e
Sven Baars <svenb.linux@gmail.com>
2025-03-31 12:54:13 +0200

    build: Account for DESTDIR in the winemetal.so install script.

--- src/winemetal/unix/install.sh
+++ src/winemetal/unix/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 UNIX_INSTALL_DIR=$2
+INSTALL_DIR="${DESTDIR%%/}${MESON_INSTALL_PREFIX}/${UNIX_INSTALL_DIR}"
 
-mkdir -p "${MESON_INSTALL_PREFIX}/${UNIX_INSTALL_DIR}"
-cp "${MESON_BUILD_ROOT}/src/winemetal/unix/winemetal.so" "${MESON_INSTALL_PREFIX}/${UNIX_INSTALL_DIR}/winemetal.so"
+mkdir -p "${INSTALL_DIR}"
+cp "${MESON_BUILD_ROOT}/src/winemetal/unix/winemetal.so" "${INSTALL_DIR}/winemetal.so"
 
 
 if [ $1 == "true" ]; then
-    strip "${MESON_INSTALL_PREFIX}/${UNIX_INSTALL_DIR}/winemetal.so" -x
+    strip "${INSTALL_DIR}/winemetal.so" -x
 fi
