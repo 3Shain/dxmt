d2604ab2bc3fa60af700cbc1ef287aa1898896b9
3Shain <san3shain@outlook.com>
2025-04-03 22:57:30 +0800

    feat(winemetal): more unixcall thunks

--- src/winemetal/Metal.hpp
+++ src/winemetal/Metal.hpp
@@ -9,6 +9,14 @@ class Object {
 public:
   obj_handle_t handle;
 
+  Object() {
+    handle = NULL_OBJECT_HANDLE;
+  }
+
+  Object(obj_handle_t h) {
+    handle = h;
+  }
+
   void
   retain() {
     NSObject_retain(handle);
@@ -25,6 +33,9 @@ concept ObjectType = std::is_base_of_v<Object, T>;
 
 template <ObjectType Class> class Reference : public Class {
 public:
+  Reference() {
+    this->handle = NULL_OBJECT_HANDLE;
+  }
   Reference(obj_handle_t retained) {
     this->handle = retained;
   }
@@ -42,6 +53,34 @@ public:
       this->release();
     this->handle = move.handle;
     move.handle = NULL_OBJECT_HANDLE;
+  }
+
+  Reference &
+  operator=(Class non_retained) {
+    this->handle = non_retained.handle;
+    this->retain();
+    return *this;
+  }
+  Reference &
+  operator=(const Reference &copy) {
+    this->handle = copy.handle;
+    this->retain();
+    return *this;
+  }
+  Reference &
+  operator=(Reference &&move) {
+    if (this->handle != NULL_OBJECT_HANDLE)
+      this->release();
+    this->handle = move.handle;
+    move.handle = NULL_OBJECT_HANDLE;
+    return *this;
+  }
+  Reference &
+  operator=(std::nullptr_t) {
+    if (this->handle != NULL_OBJECT_HANDLE)
+      this->release();
+    this->handle = NULL_OBJECT_HANDLE;
+    return *this;
   }
 
   ~Reference() {
@@ -67,8 +106,35 @@ public:
 
 class String : public Object {
 public:
-  bool getCString(char * buffer, uint64_t maxLength, WMTStringEncoding encoding) {
+  bool
+  getCString(char *buffer, uint64_t maxLength, WMTStringEncoding encoding) {
     return NSString_getCString(handle, buffer, maxLength, encoding);
+  }
+};
+
+class CommandBuffer : public Object {
+public:
+  void
+  commit() {
+    return MTLCommandBuffer_commit(handle);
+  }
+
+  void
+  waitUntilCompleted() {
+    return MTLCommandBuffer_waitUntilCompleted(handle);
+  }
+
+  WMTCommandBufferStatus
+  status() {
+    return MTLCommandBuffer_status(handle);
+  }
+};
+
+class CommandQueue : public Object {
+public:
+  CommandBuffer
+  commandBuffer() {
+    return CommandBuffer{MTLCommandQueue_commandBuffer(handle)};
   }
 };
 
@@ -88,11 +154,21 @@ public:
   name() const {
     return String{MTLDevice_name(handle)};
   }
+
+  Reference<CommandQueue>
+  newCommandQueue(uint64_t maxCommandBufferCount) {
+    return Reference<CommandQueue>(MTLDevice_newCommandQueue(handle, maxCommandBufferCount));
+  }
 };
 
 inline Reference<Array<Device>>
 CopyAllDevices() {
   return Reference<Array<Device>>(WMTCopyAllDevices());
+}
+
+inline Reference<Object>
+MakeAutoreleasePool() {
+  return Reference<Object>(NSAutoreleasePool_alloc_init());
 }
 
 } // namespace WMT
