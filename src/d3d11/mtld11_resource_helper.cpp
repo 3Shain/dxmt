f7ead093804b53307afa2b63680cfaf96ac67fc3
3Shain <san3shain@outlook.com>
2025-05-08 14:44:53 +0800

    fix(d3d11): only add atomic usage of rg32 texture if gpu supports GPUFamilyApple8

--- src/d3d11/mtld11_resource_helper.cpp
+++ src/d3d11/mtld11_resource_helper.cpp
@@ -356,7 +356,8 @@ CreateMTLTextureDescriptorInternal(
       // NOTE: R32/RG32_TYPELESS format should be mapped to uint pixel format
       if (metal_format.PixelFormat == WMTPixelFormatR32Uint ||
           metal_format.PixelFormat == WMTPixelFormatR32Sint ||
-          metal_format.PixelFormat == WMTPixelFormatRG32Uint) {
+          (metal_format.PixelFormat == WMTPixelFormatRG32Uint &&
+           pDevice->GetMTLDevice().supportsFamily(WMTGPUFamilyApple8))) {
         metal_usage |= WMTTextureUsageShaderAtomic;
       }
     }
