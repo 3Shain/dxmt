5ea3d0af385cff0600aaf26d97f50fe52763bf94
3Shain <san3shain@outlook.com>
2025-05-12 18:47:16 +0800

    feat(dxmt): decouple ring buffer management from allocation logic

--- src/dxmt/dxmt_ring_bump_allocator.hpp
+++ src/dxmt/dxmt_ring_bump_allocator.hpp
@@ -139,4 +139,210 @@ private:
   WMTBufferInfo buffer_info;
 };
 
+template <typename Allocator, size_t BlockSize = kStagingBlockSize, class mutex = dxmt::mutex> class RingBumpState {
+
+public:
+  RingBumpState(Allocator &&allocator) : allocator_(std::move(allocator)) {}
+
+  std::pair<typename Allocator::Block &, uint64_t>
+  allocate(uint64_t seq_id, uint64_t coherent_id, size_t size, size_t alignment);
+
+  void free_blocks(uint64_t coherent_id);
+
+private:
+  struct Allocation {
+    size_t allocated_size;
+    size_t total_size;
+    uint64_t last_used_seq_id;
+    Allocator::Block block;
+  };
+
+  Allocation &allocate_or_reuse_block(uint64_t seq_id, uint64_t coherent_id, size_t block_size);
+
+  std::pair<typename Allocator::Block &, uint64_t>
+  suballocate(Allocation &allocation, size_t size, size_t alignment) {
+    auto offset = align(allocation.allocated_size, alignment);
+    allocation.allocated_size = offset + size;
+    return {allocation.block, offset};
+  };
+
+  std::queue<Allocation> fifo;
+  mutex mutex_;
+  Allocator allocator_;
+};
+
+class GpuPrivateBufferBlockAllocator {
+public:
+  GpuPrivateBufferBlockAllocator(WMT::Device device, WMTResourceOptions block_options) {
+    device_ = device;
+    buffer_info_.memory.set(nullptr);
+    buffer_info_.options = block_options;
+  }
+
+  class Block {
+  public:
+    WMT::Reference<WMT::Buffer> buffer;
+    uint64_t gpu_address;
+
+    Block() = default;
+    Block(const Block &copy) = delete;
+    Block(Block &&move) = default;
+  };
+
+  Block
+  allocate(size_t block_size) {
+    Block block{};
+    buffer_info_.length = block_size;
+    block.buffer = device_.newBuffer(buffer_info_);
+    block.gpu_address = buffer_info_.gpu_address;
+    return block;
+  };
+
+private:
+  WMT::Device device_;
+  WMTBufferInfo buffer_info_;
+};
+
+class StagingBufferBlockAllocator {
+public:
+  StagingBufferBlockAllocator(WMT::Device device, WMTResourceOptions block_options) {
+    device_ = device;
+    buffer_info_ = block_options;
+  }
+
+  class Block {
+  public:
+    WMT::Reference<WMT::Buffer> buffer;
+    uint64_t gpu_address;
+    void *mapped_address;
+
+    ~Block() {
+      if (mapped_address) {
+        free(mapped_address);
+        mapped_address = nullptr;
+      }
+    };
+
+    Block() = default;
+
+    Block(Block &&move) {
+      buffer = std::move(move.buffer);
+      gpu_address = move.gpu_address;
+      mapped_address = move.mapped_address;
+      move.mapped_address = nullptr;
+    };
+  };
+
+  Block
+  allocate(size_t block_size) {
+    Block block{};
+    block.mapped_address = malloc(block_size);
+    WMTBufferInfo info;
+    info.options = buffer_info_;
+    info.memory.set(block.mapped_address);
+    info.length = block_size;
+    block.buffer = device_.newBuffer(info);
+    block.gpu_address = info.gpu_address;
+    return block;
+  };
+
+private:
+  WMT::Device device_;
+  WMTResourceOptions buffer_info_;
+};
+
+class HostBufferBlockAllocator {
+public:
+  class Block {
+  public:
+    void *ptr;
+
+    Block() = default;
+
+    Block(Block &&move) {
+      ptr = move.ptr;
+      move.ptr = nullptr;
+    };
+
+    ~Block() {
+      if (ptr) {
+        free(ptr);
+        ptr = nullptr;
+      }
+    };
+  };
+
+  Block
+  allocate(size_t block_size) {
+    Block block{};
+    block.ptr = malloc(block_size);
+    return block;
+  };
+};
+
+template <typename Allocator, size_t BlockSize, class mutex>
+std::pair<typename Allocator::Block &, uint64_t>
+RingBumpState<Allocator, BlockSize, mutex>::allocate(
+    uint64_t seq_id, uint64_t coherent_id, size_t size, size_t alignment
+) {
+  std::lock_guard<mutex> lock(mutex_);
+  while (!fifo.empty()) {
+    auto &latest = fifo.back();
+    if ((align(latest.allocated_size, alignment) + size) > latest.total_size) {
+      break;
+    }
+    latest.last_used_seq_id = seq_id;
+    return suballocate(latest, size, alignment);
+  }
+  return suballocate(
+      allocate_or_reuse_block(
+          seq_id, coherent_id, std::max(size, BlockSize) // in case required size is larger than block size
+      ),
+      size, alignment
+  );
+};
+
+template <typename Allocator, size_t BlockSize, class mutex>
+void
+RingBumpState<Allocator, BlockSize, mutex>::free_blocks(uint64_t coherent_id) {
+  std::lock_guard<mutex> lock(mutex_);
+  while (!fifo.empty()) {
+    auto &front = fifo.front();
+    if (front.last_used_seq_id <= coherent_id && (coherent_id - front.last_used_seq_id) > kStagingBlockLifetime) {
+      // can be deallocated
+      fifo.pop();
+    } else {
+      break;
+    }
+  }
+};
+
+template <typename Allocator, size_t BlockSize, class mutex>
+RingBumpState<Allocator, BlockSize, mutex>::Allocation &
+RingBumpState<Allocator, BlockSize, mutex>::allocate_or_reuse_block(
+    uint64_t seq_id, uint64_t coherent_id, size_t block_size
+) {
+  if (!fifo.empty()) {
+    auto &front = fifo.front();
+    if (front.last_used_seq_id < coherent_id) {
+      if (front.total_size >= block_size) {
+        front.last_used_seq_id = seq_id;
+        front.allocated_size = 0;
+        fifo.push(std::move(front));
+        fifo.pop();
+        return fifo.back();
+      } else {
+        ERR("forced to allocate new block of size ", block_size);
+      }
+    }
+  }
+  fifo.push({
+      .allocated_size = 0,
+      .total_size = block_size,
+      .last_used_seq_id = seq_id,
+      .block = allocator_.allocate(block_size),
+  });
+  return fifo.back();
+};
+
 } // namespace dxmt
