a4cb660e6a2718a2b521ac30b922303224891eef
3Shain <san3shain@outlook.com>
2025-06-13 03:45:16 +0800

    feat(dxmt): defer layer props setting and always pull pseudo display setting

--- src/dxmt/dxmt_presenter.hpp
+++ src/dxmt/dxmt_presenter.hpp
@@ -14,17 +14,27 @@ public:
 
   bool changeLayerColorSpace(WMTColorSpace colorspace);
 
+  void checkDisplaySetting();
+
+  void changeHDRMetadata(const WMTHDRMetadata *metadata);
+
   WMT::MetalDrawable encodeCommands(WMT::CommandBuffer cmdbuf, WMT::Texture backbuffer);
 
 private:
-  void buildRenderPipelineState();
+  void buildRenderPipelineState(bool is_pq, bool with_hdr_metadata);
 
   WMT::Device device_;
   WMT::MetalLayer layer_;
   InternalCommandLibrary &lib_;
   WMTLayerProps layer_props_;
   WMTColorSpace colorspace_ = WMTColorSpaceSRGB;
+  WMTHDRMetadata hdr_metadata_;
+  bool has_hdr_metadata_ = false;
   WMTPixelFormat source_format_ = WMTPixelFormatInvalid;
+  uint64_t display_setting_version_ = 0;
+  WMTColorSpace display_colorspace_ = WMTColorSpaceSRGB;
+  WMTHDRMetadata display_hdr_metadata_;
+  WMTEDRValue display_edr_value_{0.0, 1.0};
   WMT::Reference<WMT::RenderPipelineState> present_blit_;
   WMT::Reference<WMT::RenderPipelineState> present_scale_;
   std::atomic_flag pso_valid = 0;
