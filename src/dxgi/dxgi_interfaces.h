4d457ad292ca5a15870e7f853926bccc75dd7ece
Alexander Eichner <alexander.eichner@oracle.com>
2025-07-03 13:07:35 +0200

    dxmt/native: Make dxgi build

--- src/dxgi/dxgi_interfaces.h
+++ src/dxgi/dxgi_interfaces.h
@@ -9,6 +9,11 @@ DEFINE_COM_INTERFACE("acdf3ef1-b33a-4cb6-97bd-1c1974827e6d", IMTLDXGIAdapter)
   virtual WMT::Device STDMETHODCALLTYPE GetMTLDevice() = 0;
 };
 
+#ifdef DXMT_NATIVE
+DEFINE_GUID(IID_IMTLDXGIAdapter, 0xacdf3ef1, 0xb33a, 0x4cb6, 0x97, 0xbd, 0x1c, 0x19, 0x74, 0x82, 0x7e, 0x6d);
+__CRT_UUID_DECL(IMTLDXGIAdapter, 0xacdf3ef1, 0xb33a, 0x4cb6, 0x97, 0xbd, 0x1c, 0x19, 0x74, 0x82, 0x7e, 0x6d);
+#endif
+
 DEFINE_COM_INTERFACE("6bfa1657-9cb1-471a-a4fb-7cacf8a81207", IMTLDXGIDevice)
     : public IDXGIDevice3 {
   virtual WMT::Device STDMETHODCALLTYPE GetMTLDevice() = 0;
@@ -17,6 +22,11 @@ DEFINE_COM_INTERFACE("6bfa1657-9cb1-471a-a4fb-7cacf8a81207", IMTLDXGIDevice)
       const DXGI_SWAP_CHAIN_FULLSCREEN_DESC *pFullscreenDesc,
       IDXGISwapChain1 **ppSwapChain) = 0;
 };
+
+#ifdef DXMT_NATIVE
+DEFINE_GUID(IID_IMTLDXGIDevice, 0x6bfa1657, 0x9cb1, 0x471a, 0xa4, 0xfb, 0x7c, 0xac, 0xf8, 0xa8, 0x12, 0x07);
+__CRT_UUID_DECL(IMTLDXGIDevice, 0x6bfa1657, 0x9cb1, 0x471a, 0xa4, 0xfb, 0x7c, 0xac, 0xf8, 0xa8, 0x12, 0x07);
+#endif
 
 static constexpr IID DXMT_NVEXT_GUID = dxmt::guid::make_guid("ba0af616-4a43-4259-815c-db3b89829905");
 
