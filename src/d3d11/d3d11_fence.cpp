fd2b382e202909f198e3a0bd8f03c34d3fd425a3
Marc-Aurel Zent <mzent@codeweavers.com>
2025-11-17 09:41:47 +0100

    feat(d3d11): implement `OpenSharedFence`

--- src/d3d11/d3d11_fence.cpp
+++ src/d3d11/d3d11_fence.cpp
@@ -139,7 +139,54 @@ CreateFence(MTLD3D11Device *pDevice, UINT64 InitialValue,
 HRESULT
 OpenSharedFence(MTLD3D11Device *pDevice, HANDLE hResource,
                 REFIID riid, void **ppFence) {
-  return E_FAIL; // Not implemented yet
+  InitReturnPtr(ppFence);
+
+  if (reinterpret_cast<uintptr_t>(hResource) & 0xc0000000) {
+    WARN("OpenSharedFence: Invalid shared handle type");
+    return E_INVALIDARG;
+  }
+
+  if (ppFence == nullptr)
+    return S_FALSE;
+
+  char mach_port_name[54];
+
+  D3DKMT_QUERYRESOURCEINFOFROMNTHANDLE query = {};
+  query.hDevice = pDevice->GetLocalD3DKMT();
+  query.hNtHandle = hResource;
+  query.pPrivateRuntimeData = mach_port_name;
+  query.PrivateRuntimeDataSize = sizeof(mach_port_name);
+
+  if (D3DKMTQueryResourceInfoFromNtHandle(&query)) {
+    WARN(str::format("OpenSharedFence: Failed to query resource: ", hResource));
+    return E_INVALIDARG;
+  }
+
+  if (query.PrivateRuntimeDataSize != sizeof(mach_port_name)) {
+    WARN(str::format("OpenSharedFence: Unexpected size: ", query.PrivateRuntimeDataSize));
+    return E_INVALIDARG;
+  }
+
+  D3DKMT_OPENSYNCOBJECTFROMNTHANDLE2 open = {};
+  open.hDevice = pDevice->GetLocalD3DKMT();
+  open.hNtHandle = hResource;
+
+  if (D3DKMTOpenSyncObjectFromNtHandle2(&open)) {
+    WARN(str::format("OpenSharedFence: Failed to open resource: ", hResource));
+    return E_INVALIDARG;
+  }
+
+  mach_port_t mach_port;
+  if (!WMTBootstrapLookUp(mach_port_name, &mach_port)) {
+    ERR("ImportSharedTexture: Failed to look up mach port");
+    return E_INVALIDARG;
+  }
+
+  auto fence = new MTLD3D11FenceImpl(
+      pDevice,
+      pDevice->GetMTLDevice().newSharedEventWithMachPort(mach_port),
+      open.hSyncObject);
+  return fence->QueryInterface(riid, ppFence);
 }
 
 } // namespace dxmt
