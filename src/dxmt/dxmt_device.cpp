2130c92d584496aa61f99508d22521beb75a1689
Feifan He <san3shain@outlook.com>
2025-12-10 07:58:18 +0800

    fix(dxmt): avoid crash in `setShouldMaximizeConcurrentCompilation` on macOS 26 with AMDGPU

--- src/dxmt/dxmt_device.cpp
+++ src/dxmt/dxmt_device.cpp
@@ -25,8 +25,7 @@ public:
   };
 
   DeviceImpl(const DEVICE_DESC &desc) : device_(desc.device), cmd_queue_(device_) {
-    uint64_t macos_major_version = 0;
-    device_.setShouldMaximizeConcurrentCompilation(true);
+    uint64_t macos_major_version = 0, macos_minor_version = 0;
     int version_conf = Config::getInstance().getOption<int>("dxmt.shaderMetalVersion", 0);
     switch (version_conf) {
     case WMTMetal310:
@@ -37,7 +36,7 @@ public:
       metal_version_ = WMTMetalVersionMax;
       break;
     }
-    WMTGetOSVersion(&macos_major_version, nullptr, nullptr);
+    WMTGetOSVersion(&macos_major_version, &macos_minor_version, nullptr);
     if (macos_major_version >= 15) {
       metal_version_ = std::min(WMTMetal320, metal_version_);
     } else {
@@ -47,8 +46,12 @@ public:
       WARN("Experimental non-Apple GPU support");
       metal_version_ = WMTMetal310; // Metal 3.2 features we need are basically not available
       max_object_threadgroups_ = 1024;
+      // macOS 26 bug: setShouldMaximizeConcurrentCompilation crashes on AMDGPU
+      if (!(macos_major_version >= 16 && macos_major_version <= 26 && macos_minor_version < 2))
+        device_.setShouldMaximizeConcurrentCompilation(true);
     } else {
       max_object_threadgroups_ = -1ull;
+      device_.setShouldMaximizeConcurrentCompilation(true);
     }
   }
 
