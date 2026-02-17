7023736d586e68fe3a8815a612882e844a926812
Feifan He <san3shain@outlook.com>
2026-02-17 16:44:48 +0800

    build: bump llvm-mingw to 20251216

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ if test -z "$NATIVE_BUILD"; then
   tar -zvxf ./wine.tar.gz -C ./toolchains/wine
   rm ./wine.tar.gz
   # install mingw-llvm
-  curl -SL https://github.com/mstorsjo/llvm-mingw/releases/download/20231017/llvm-mingw-20231017-ucrt-macos-universal.tar.xz > ./llvm.tar.xz
+  curl -SL https://github.com/mstorsjo/llvm-mingw/releases/download/20251216/llvm-mingw-20251216-ucrt-macos-universal.tar.xz > ./llvm.tar.xz
   tar -zvxf ./llvm.tar.xz -C ./toolchains
   rm ./llvm.tar.xz
 fi
@@ -54,7 +54,7 @@ else
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_TARGETS_TO_BUILD="" \
     -DLLVM_BUILD_TOOLS=Off \
-    -DCMAKE_SYSROOT="$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal" \
+    -DCMAKE_SYSROOT="$(pwd)/toolchains/llvm-mingw-20251216-ucrt-macos-universal" \
     -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
     -DBUG_REPORT_URL="https://github.com/3Shain/dxmt" \
     -DPACKAGE_VENDOR="DXMT" \
@@ -86,7 +86,7 @@ else
 fi
 
 ## configure
-# export PATH="$PATH:$(pwd)/toolchains/llvm-mingw-20231017-ucrt-macos-universal/bin"
+# export PATH="$PATH:$(pwd)/toolchains/llvm-mingw-20251216-ucrt-macos-universal/bin"
 # meson setup --cross-file build-win64.txt --native-file build-osx.txt build
 ## build
 # meson compile -C build
