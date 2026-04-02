5182d54df22c6553cd15f207f89160e00dfddc4a
Feifan He <san3shain@outlook.com>
2026-04-02 20:55:27 +0800

    refactor(d3d11): devirtualize SRV and UAV common accessors
    Actually we can just access the data member and get rid of accessors, but it also leads to large amount of noisy changes. Compilers will optimize it as simple data load anyway.

--- src/d3d11/d3d11_view.hpp
+++ src/d3d11/d3d11_view.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "d3d11_1.h"
+#include "d3d11_3.h"
 #include "dxmt_buffer.hpp"
 #include "dxmt_texture.hpp"
 
@@ -14,18 +14,56 @@ struct MTL_RENDER_PASS_ATTACHMENT_DESC {
 namespace dxmt {
 
 struct D3D11ShaderResourceView : ID3D11ShaderResourceView1 {
-  virtual Rc<Buffer> buffer() = 0;
-  virtual BufferSlice bufferSlice() = 0;
-  virtual Rc<Texture> texture() = 0;
-  virtual unsigned viewId() = 0;
+  Buffer *buffer_{};
+  BufferSlice slice_{};
+  Texture *texture_{};
+  unsigned view_id_{};
+
+  Rc<Buffer>
+  buffer() const {
+    return buffer_;
+  };
+  BufferSlice
+  bufferSlice() const {
+    return slice_;
+  };
+  Rc<Texture>
+  texture() const {
+    return texture_;
+  };
+  unsigned
+  viewId() const {
+    return view_id_;
+  };
 };
 
 struct D3D11UnorderedAccessView : ID3D11UnorderedAccessView1 {
-  virtual Rc<Buffer> buffer() = 0;
-  virtual BufferSlice bufferSlice() = 0;
-  virtual Rc<Texture> texture() = 0;
-  virtual unsigned viewId() = 0;
-  virtual Rc<Buffer> counter() = 0;
+  Buffer *buffer_{};
+  BufferSlice slice_{};
+  Texture *texture_{};
+  unsigned view_id_{};
+  Rc<Buffer> counter_;
+
+  Rc<Buffer>
+  buffer() const {
+    return buffer_;
+  };
+  BufferSlice
+  bufferSlice() const {
+    return slice_;
+  };
+  Rc<Texture>
+  texture() const {
+    return texture_;
+  };
+  unsigned
+  viewId() const {
+    return view_id_;
+  };
+  Rc<Buffer>
+  counter() const {
+    return counter_;
+  };
 };
 
 struct D3D11RenderTargetView : ID3D11RenderTargetView1 {
