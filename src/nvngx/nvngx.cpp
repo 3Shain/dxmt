1da35e20ce79e05890cf5575ce6a6cc737cf608c
Feifan He <san3shain@outlook.com>
2026-04-20 13:16:31 +0800

    feat(nvngx): add stub for `GetScratchBufferSize()`

--- src/nvngx/nvngx.cpp
+++ src/nvngx/nvngx.cpp
@@ -11,11 +11,6 @@
 namespace dxmt {
 Logger Logger::s_instance("nvngx.log");
 
-#define IMPLEMENT_ME                                                                                                   \
-  Logger::err(str::format(__FILE__, ":", __FUNCTION__, " is not implemented."));                                       \
-  abort();                                                                                                             \
-  __builtin_unreachable();
-
 NVNGX_API NVNGX_RESULT
 NVSDK_NGX_D3D11_Init_Ext(
     unsigned long long id, const wchar_t *path, ID3D11Device *device, unsigned int sdk_version, const void *feature_info
@@ -171,8 +166,9 @@ NVSDK_NGX_D3D11_Shutdown1(ID3D11Device *device) {
 
 NVNGX_API NVNGX_RESULT
 NVSDK_NGX_D3D11_GetScratchBufferSize(unsigned int feature, const NVNGXParameter *params, size_t *out_size) {
-  IMPLEMENT_ME
-  return NVNGX_RESULT_FAIL;
+  if (out_size)
+    *out_size = 0;
+  return NVNGX_RESULT_OK;
 }
 
 NVNGX_API NVNGX_RESULT
