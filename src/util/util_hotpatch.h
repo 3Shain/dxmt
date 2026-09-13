28ba148fd944a97fb25a5e00d90d002e6a9844f3
Feifan He <feifan@codeweavers.com>
2026-09-13 17:13:13 +0800

    chore(util): define `DXMT_HOTPATCHABLE`

---
+++ src/util/util_hotpatch.h
@@ -0,0 +1,24 @@
+/*
+ * Derived from DXVK (originally under zlib License)
+ *
+ * See
+ * <https://github.com/doitsujin/dxvk/blob/8c4fd2723c031b425fb857454e9e5fa4ca25f4ac/src/util/util_hotpatch.h>
+ * See <https://github.com/doitsujin/dxvk/blob/master/LICENSE>
+ */
+
+#pragma once
+
+#ifndef __has_attribute
+#define __has_attribute(attr) 0
+#endif
+
+#if __has_attribute(ms_hook_prologue) &&                                       \
+    (defined(__i386__) || defined(__x86_64__))
+#define DXMT_HOTPATCHABLE __attribute__((__ms_hook_prologue__))
+#elif defined(DECLSPEC_CHPE_PATCHABLE)
+#define DXMT_HOTPATCHABLE DECLSPEC_CHPE_PATCHABLE
+#elif defined(__arm64ec__)
+#define DXMT_HOTPATCHABLE __attribute__((hybrid_patchable))
+#else
+#define DXMT_HOTPATCHABLE
+#endif
